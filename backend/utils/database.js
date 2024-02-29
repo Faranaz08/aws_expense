21a2be14845f50e59931e1b19d83181adddacdf8
Faranaz08 <faranazannigeri3142@gmail.com>
2024-03-01 02:11:31 +0530

    all env pushedd separtely for aws

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("expenseproj", "root", "faranaz", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_SCHEMA,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 module.exports = sequelize;
