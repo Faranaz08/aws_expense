8b07daa15e3c3d97cd7b0c9dbf1c41d3f42ab433
Faranaz08 <faranazannigeri3142@gmail.com>
2024-03-01 20:16:36 +0530

    aws final

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -3,7 +3,7 @@ const dotenv=require("dotenv");
 
 //get config vars
 dotenv.config();
-
+//
 
 const sequelize = new Sequelize(process.env.DB_SCHEMA,process.env.DB_USER,process.env.DB_PASSWORD,
   {
